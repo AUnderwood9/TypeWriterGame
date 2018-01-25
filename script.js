dac053e6e47a05a219992a4e6d66d16f2030ed93
AUnderwood9 <aunderwood797@gmail.com>
2018-01-24 22:25:47 -0600

    Added character highlighting on the keyboard for special characters as well as characters in the upper-container

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 $(document).ready(() => {
-    let keyShifted = false;
-    $(document).bind("keydown", function( event ) {
-        let currentKeyCode = event.keyCode;
+    $(document).on("keydown", function( event ) {
+        let currentKeyCode = event.which;
+        let shiftPressed = event.shiftKey;
         console.log(currentKeyCode);
-        //console.log(currentKeyCode);
-        if ( event.keyCode == 16 ) {
-            keyShifted = true;
-            $("#keyboard-upper-container").toggle();
-            $("#keyboard-lower-container").toggle();
-        }else if(keyShifted){
-            
+        if ( shiftPressed ) {
+            $("#keyboard-upper-container").show();
+            $("#keyboard-lower-container").hide();
+            //check if ascii are numbers or letters
+            if(!(currentKeyCode === 32) || !(currentKeyCode <= 122 && currentKeyCode >= 97))
+            {
+                $("#shift" + currentKeyCode).css("background-color", "yellow");
+                console.log("shifted");
+            }
+            $("#" + currentKeyCode).css("background-color", "yellow");
         }else {
-            let characterCode = (event.keyCode + 32) <= 122 || (event.keyCode + 32) >= 97 ? (event.keyCode + 32) : event.keyCode; 
-            console.log((event.keyCode + 32) <= 122 || (event.keyCode + 32) >= 97 ? (event.keyCode + 32) : event.keyCode);
+            let characterCode = (currentKeyCode + 32) <= 122 && (currentKeyCode + 32) >= 97 ? (currentKeyCode + 32) : currentKeyCode; 
+            console.log((currentKeyCode + 32) <= 122 && (currentKeyCode + 32) >= 97 ? (currentKeyCode + 32) : currentKeyCode);
             $("#" + characterCode).css("background-color", "yellow");
         }
-    }),$(document).bind("keyup", function( event ) {
-        let currentKeyCode = event.keyCode;
-        if ( event.keyCode == 16 ) {
-            keyShifted = false;
-            $("#keyboard-upper-container").toggle();
-            $("#keyboard-lower-container").toggle();
-        }else if(keyShifted){
-            
-        }else{
-            let characterCode = (event.keyCode + 32) <= 122 || (event.keyCode + 32) >= 97 ? (event.keyCode + 32) : event.keyCode; 
+    }),$(document).on("keyup", function( event ) {
+        let currentKeyCode = event.which;
+        let shiftPressed = event.shiftKey;
+        if ( !shiftPressed ) {
+            $("#keyboard-upper-container").hide();
+            $("#keyboard-lower-container").show();
+            let characterCode = (currentKeyCode + 32) <= 122 && (currentKeyCode + 32) >= 97 ? (currentKeyCode + 32) : currentKeyCode; 
             $("#" + characterCode).removeAttr("style");
+        }else if(shiftPressed){
+            //check if ascii are numbers or letters
+            if(!(currentKeyCode === 32) || !(currentKeyCode <= 122 && currentKeyCode >= 97))
+            {
+                $("#shift" + currentKeyCode).removeAttr("style");
+            }
+            $("#" + currentKeyCode).removeAttr("style");
+        }else{
+            
         }
     });
 
